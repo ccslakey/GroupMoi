3c9fa7491a35cf902c0dadc554ebf7c8e3be4244
ccslakey <cromwellslakey@gmail.com>
2015-11-07 09:46:38 -0800

    methods, event handlers, and comments

--- GroupMoi.js
+++ GroupMoi.js
@@ -12,6 +12,79 @@ if (Meteor.isClient) {
       }
   });
 
+  Template.body.events({
+      "submit .new-group": function (event) {
+          // Grab group name from text field
+          var newGroup = event.target.group.value;
+          // Check that text field is not blank before adding group
+          if (newGroup !== '') {
+              Meteor.call("addGroup", newGroup);
+          }
+          // Clear the text field for next entry
+          event.target.group.value = "";
+          // Prevent default form submit
+          return false;
+      },
+      "submit .new-number": function (event) {
+          // Grab phone number from text field
+          var newNumber = event.target.number.value;
+          // Check that text field is not blank before adding number
+          if (newNumber !== '') {
+              Meteor.call("addNumber", this._id, newNumber);
+          }
+          // Clear the text field for next entry
+          event.target.number.value = "";
+          // Prevent default form submit
+          return false;
+      },
+      "submit .new-text": function (event) {
+          // Grab text message from text field
+          var newMessage = event.target.message.value;
+          // Check that message field is not blank before sending texts
+          if (newMessage !== '') {
+              Meteor.call("sendMessage", newMessage);
+          }
+          // Clear the text field
+          event.target.message.value = "";
+          alert('Your message is being sent!');
+          // Prevent default form submit
+          return false;
+      }
+  });
+  
+
+
+  /*
+  These four methods make up the toggling functionality 
+  that will allow a user to select/deselect 
+  individual groups or numbers when blasting out a text
+  */
+  Template.group.events({
+      "click .toggle-group": function () {
+          // Set the checked property to the opposite of its current value
+          Meteor.call("toggleGroup", this._id, !this.checked);
+      },
+      "click .toggle-number": function () {
+          // Get the number's group data
+          var data = Template.instance().data;
+          // Set the checked property to the opposite of its current value
+          Meteor.call("toggleNumber", data._id, this.number, !this.checked);
+      },
+      "click .delete-group": function () {
+          // Remove a group from our collection
+          Meteor.call("deleteGroup", this._id);
+      },
+      "click .delete-number": function () {
+          // Get the number's group data
+          var group = Template.instance().data;
+          // Remove a number from a particular group
+          Meteor.call("deleteNumber", group._id, this.number);
+      }
+  });
+
+
+
+
 // end of client
 }
 
@@ -22,6 +95,39 @@ if (Meteor.isServer) {
             owner: this.userId
         });
     });
+
+
+    Meteor.methods({
+
+      /*
+      addGroup will insert a new group into our Groups
+      collection. By default, new groups will 
+      be empty and will not be selected.
+      */
+        addGroup: function (name) {
+            Groups.insert({
+                name: name,
+                createdAt: new Date(),
+                owner: Meteor.userId(),
+                checked: false,
+                numbers: []
+            });
+        },
+        /*
+        addNumber will insert a new number into a 
+        group’s numbers array. By default, new numbers
+        will be selected.
+        */
+        addNumber: function (groupId, number) {
+            Groups.update(
+                {_id: groupId},
+                {$addToSet: {numbers: {"number": number, "checked": true }}}
+            );
+        }
+    });
+
+
+
 
 // end of server
 }
